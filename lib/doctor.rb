59cca367d2d92327c989b5b1fa8c2b2129e54f53
virtual-frame-7789 <mttwright@gmail.com>
2019-10-13 21:00:33 +0000

    Automatically backed up by Learn

---
+++ lib/doctor.rb
@@ -0,0 +1,15 @@
+class Doctor
+  
+  attr_reader :name
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @@all.push(self)
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def new_appointment(patient)
